b13047f92995ed9d952649ce80739bc037320780
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2019-09-15 19:46:53 +0200

    Cleanup java stuff

--- .dotfiles/java.sh
+++ .dotfiles/java.sh
@@ -1,15 +1,21 @@
-# Android SDK (via Homebrew)
+# Android SDK
 export JAVA_HOME=$(/usr/libexec/java_home)
-export ANT_HOME=/usr/local/opt/ant
-export MAVEN_HOME=/usr/local/opt/maven
-export GRADLE_HOME=/usr/local/opt/gradle
-export ANDROID_HOME=/Users/lucassus/Library/Android/sdk
-
 export PATH=${JAVA_HOME}/bin:$PATH
+
+export ANT_HOME=/usr/local/opt/ant
 export PATH=$ANT_HOME/bin:$PATH
+
+export MAVEN_HOME=/usr/local/opt/maven
 export PATH=$MAVEN_HOME/bin:$PATH
+
+export GRADLE_HOME=/usr/local/opt/gradle
 export PATH=$GRADLE_HOME/bin:$PATH
+
+# Android stuff
+export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$ANDROID_HOME/tools:$PATH
 export PATH=$ANDROID_HOME/platform-tools:$PATH
 export PATH=$ANDROID_HOME/build-tools/28.0.3:$PATH
+
+# Expose fastlane
 export PATH=$HOME/.fastlane/bin:$PATH
