009d4375c5af7c9fa84b9082f389b56027fab5a7
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2019-09-16 08:41:01 +0200

    Install iterm2 shell integration

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@ if [[ ! -s "$HOME/antigen.zsh" ]]; then
   curl -L git.io/antigen > "$HOME/antigen.zsh"
 fi
 
+# Install iterm2 shell integration
+if [[ ! -s "$HOME/.iterm2_shell_integration.zsh" ]]; then
+  curl -L https://iterm2.com/shell_integration/zsh -o "$HOME/.iterm2_shell_integration.zsh"
+fi
+
 # Install required brew packages
 brew_packages=(fasd fzf direnv)
 
