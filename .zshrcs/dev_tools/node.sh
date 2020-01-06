987a1a354572cc1680b96418dc7cbbd306628191
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-01-06 11:42:30 +0100

    Install nvm via brew

--- .zshrcs/dev_tools/node.sh
+++ .zshrcs/dev_tools/node.sh
@@ -1,6 +1,6 @@
 # Load node version manager
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
+source $(brew --prefix nvm)/nvm.sh
 
 # Autoswitch nodejs version
 function __use_project_node() {
