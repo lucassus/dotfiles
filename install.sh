60df004732d4a1a2f9dfe7c0e320a2b7bcd16dda
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-01-08 23:02:37 +0100

    Remove obsolete brew packages install

--- install.sh
+++ install.sh
@@ -3,12 +3,6 @@ if [[ ! -s "$HOME/.iterm2_shell_integration.zsh" ]]; then
   curl -L https://iterm2.com/shell_integration/zsh -o "$HOME/.iterm2_shell_integration.zsh"
 fi
 
-for package in "${brew_packages[@]}"; do
-  if ! type "$package" &> /dev/null; then
-    brew install "$package"
-  fi
-done
-
 # Copy files to the home folder
 files=(.zshrcs bin .gitconfig .zshrc)
 
